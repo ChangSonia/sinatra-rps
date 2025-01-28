27259f56a15318dfea7e1dd64b9bb8cd67263eac
ChangSonia <79300461+ChangSonia@users.noreply.github.com>
2025-01-28 03:37:37 +0000

    first attempt

--- app.rb
+++ app.rb
@@ -1,9 +1,52 @@
 require "sinatra"
 require "sinatra/reloader"
 
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home,{:layout => :wrapper})
+end
+
+
+# Play rock
+get("/rock") do
+  moves = ["rock","paper","scissor"]
+  @opponent = moves.sample
+  if @opponent == "rock"
+    @outcome = "tied"
+  elsif @opponent == "paper"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  erb(:rock,{:layout => :wrapper})
+end
+
+
+# Play paper
+get("/paper") do
+  moves = ["rock","paper","scissor"]
+  @opponent = moves.sample
+  if @opponent == "paper"
+    @outcome = "tied"
+  elsif @opponent == "scissor"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  erb(:paper,{:layout => :wrapper})
+end
+
+
+# Play scissor
+get("/scissor") do
+  moves = ["rock","paper","scissor"]
+  @opponent = moves.sample
+  if @opponent == "scissor"
+    @outcome = "tied"
+  elsif @opponent == "rock"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  erb(:scissor,{:layout => :wrapper})
 end
